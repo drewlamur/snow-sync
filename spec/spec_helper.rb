f56d7cf9c4b412e8203a974bfa3b93269534686b
drewlamur <sailingthetechseas@gmail.com>
2022-01-23 13:41:46 +0000

    rspec helper, configuring color and formatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ RSpec.configure do |config|
   require "json"
   require_relative "../lib/snow_sync/sync_util.rb"
 
+  config.color
+  config.formatter = :documentation
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
